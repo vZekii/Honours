a5ef1d18fb4b57af3e16fe223b1b3369e2033bd1
Zekii <zach7505@gmail.com>
2023-09-19 17:16:38 +1000

    Start of conversion from gaps test to implementation into sabre

--- gaps_test.py
+++ gaps_test.py
@@ -6,10 +6,14 @@ from qiskit.transpiler.layout import Layout
 
 from collections import defaultdict
 
-from pprint import pprint
 from rich import print  # fancier colouring
+from rich.panel import Panel
 
 from enum import Enum
+
+EXTENDED_SET_SIZE = (
+    20  # Size of lookahead window. TODO: set dynamically to len(current_layout)
+)
 
 
 # Dummy putting gate here as it will be used to handle single gates later on
@@ -35,7 +39,34 @@ def _build_required_predecessors(dag):
     return out
 
 
-def apply_gate_commutative(gate):
+def _obtain_extended_set(dag, front_layer, pred):
+    """Populate extended_set by looking ahead a fixed number of gates.
+    For each existing element add a successor until reaching limit.
+    """
+    extended_set = []
+    decremented = []
+    tmp_front_layer = front_layer
+    done = False
+    while tmp_front_layer and not done:
+        new_tmp_front_layer = []
+        for node in tmp_front_layer:
+            for successor in _successors(node, dag):
+                decremented.append(successor)
+                pred[successor] -= 1
+                if pred[successor] == 0:
+                    new_tmp_front_layer.append(successor)
+                    if len(successor.qargs) == 2:
+                        extended_set.append(successor)
+            if len(extended_set) >= EXTENDED_SET_SIZE:
+                done = True
+                break
+        tmp_front_layer = new_tmp_front_layer
+    for node in decremented:
+        pred[node] += 1
+    return extended_set
+
+
+def apply_gate_commutative(gate, dag, front_layer, pred):
     """
     The bread and butter of the new algorithm. Will attempt to apply gates with commutativity, otherwise build a storage list of up to one gate.
     """
@@ -47,7 +78,12 @@ def apply_gate_commutative(gate):
         current_layout._v2p[gate.qargs[1]],
     )
 
-    print(f"Attempting application of {gate.name} gate on qubits {v0} and {v1}")
+    print(
+        Panel(
+            f"Attempting application of {gate.name} gate on qubits {v0} and {v1}",
+            highlight=True,
+        )
+    )
     print(gap_storage)
 
     for i in range(min(v0, v1) + 1, max(v0, v1)):
@@ -58,10 +94,17 @@ def apply_gate_commutative(gate):
     if gap_storage[v0] == Gap.CONTROL and gap_storage[v1] != Gap.TARGET:
         print(f"Found potential control match on qubit {v0}")
 
-    if gap_storage[v1] == Gap.TARGET and gap_storage[v0] != Gap.CONTROL:
+    elif gap_storage[v1] == Gap.TARGET and gap_storage[v0] != Gap.CONTROL:
         print(f"Found potential target match on qubit {v0}")
 
+    else:
+        gate_storage[v0], gate_storage[v1] = gate, gate
+
     gap_storage[v0], gap_storage[v1] = Gap.CONTROL, Gap.TARGET
+
+    print(_obtain_extended_set(dag, front_layer, pred))
+
+    print(gate_storage)
 
 
 # Setup circuit
@@ -72,14 +115,15 @@ dag_in = circuit_to_dag(circuit_in)
 # draw_circuit(circuit_in, "testing")
 draw_dag(dag_in, filename="testdag.png")  # No graphviz on my laptop
 print(dag_in.properties())
-
-# Test code
 pred = _build_required_predecessors(dag_in)
-pprint(pred)
 
 canonical_register = dag_in.qregs["q"]
 current_layout = Layout.generate_trivial_layout(canonical_register)
 print(current_layout)
+
+# Preserve input DAG's name, regs, wire_map, etc. but replace the graph.
+mapped_dag = dag_in.copy_empty_like()
+
 
 physical_qubits = [0, 1, 2, 3, 4, 5]
 gap_storage = {
@@ -90,6 +134,7 @@ gate_storage = {idx: [] for idx in physical_qubits}  # dictionary to store 2 qub
 
 # Execution
 front_layer = dag_in.front_layer()
+num_search_steps = 0
 
 # * this is loosely copied from SABRE
 while front_layer:
@@ -97,12 +142,16 @@ while front_layer:
 
     for gate in front_layer:
         if len(gate.qargs) == 2:
+            # coupling check has been left out here
             execute_list.append(gate)
+
+        else:
+            execute_list.append(gate)  # fallback for single qubit gates
 
     front_layer = []
     if execute_list:
         for gate in execute_list:
-            apply_gate_commutative(gate)
+            apply_gate_commutative(gate, dag_in, front_layer, pred)
 
             for successor in _successors(gate, dag_in):
                 pred[successor] -= 1
@@ -112,5 +161,7 @@ while front_layer:
         ops_since_progress = []
         extended_set = None
         continue
+
+    num_search_steps += 1
 
     print("Got to the end")
